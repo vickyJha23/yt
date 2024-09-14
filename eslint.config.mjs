7da4ded5446ff4869974d7bf0b07f487c097dc36
vickyJha23 <vickyjha388@gmail.com>
2024-09-14 18:25:44 +0530

    add:husky

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default tseslint.config({
+    extends:[
+        eslint.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
+    ...tseslint.configs.stylisticTypeChecked,
+    eslintConfigPrettier
+    
+    ],
+    
+      languageOptions: {
+        parserOptions: {
+          projectService: true,
+          tsconfigRootDir: import.meta.dirname,
+        },
+      },
+    
+    
+      files: ['**/*.ts'],
+    }
+);
